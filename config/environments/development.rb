b957848e2f0237bda3d3ab119d23d5c7fcc92d10
fersubair40 <olajnrsubair@gmail.com>
2019-10-07 11:18:48 +0100

    yay

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.logger = Logger.new(STDOUT)
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
